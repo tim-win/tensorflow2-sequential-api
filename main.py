0c32da5239a89e643d65398cdca8bfbe3e0bc543
tim-win <tim@timsthebomb.com>
2019-09-15 16:44:58 -0400

    Add placeholder code for main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-import sys
-import tensorflow as tf
-from tensorflow import keras
-
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
-
+# Main.py in the tensorflow-2-test repo.
+# Replace with useful ML code!
 
 def main():
-    fashion_mnist = keras.datasets.fashion_mnist
+# The following is stolen verbatim  from
+# https://www.tensorflow.org/tutorials
 
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-    return 0
+# Replace with something actually useful
+    import tensorflow as tf
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(512, activation=tf.nn.relu),
+      tf.keras.layers.Dropout(0.2),
+      tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+    model.evaluate(x_test, y_test)
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
