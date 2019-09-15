4b59f8525c03eb9c95657991d74c335611282ea4
tim-win <tim@timsthebomb.com>
2019-09-15 16:37:34 -0400

    Cleanup of initial implementation

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
 FROM nvidia/cuda:10.0-cudnn7-runtime-centos7
 
-CMD ["python3", "/opt/workspace/main.py"]
+CMD ["python3.6", "/opt/workspace/main.py"]
 
 RUN mkdir -p /opt/workspace /opt/data
-
-
 WORKDIR /opt/workspace
 
-RUN yum install -y epel-release
+
+RUN yum install -y epel-release gcc
 RUN yum install -y python36-devel python36-pip
-RUN pip3 install tensorflow-gpu==2.0.0-rc1
+RUN pip3.6 install --upgrade pip
 
+RUN pip3 install 'tensorflow-gpu==2.0.0rc1'
 
-RUN yum install -y gcc
 COPY ./requirements.txt /opt/requirements.txt
 RUN pip3 install -r /opt/requirements.txt
 
--- run.sh
+++ run.sh
@@ -3,18 +3,19 @@ set -exuo pipefail
 IFS=$'\n\t'
 
 NAME="tf2_test"
+IMG_NAME="tft"
+TAG="test"
+IMG="${IMG_NAME}:${TAG}"
 set +eo pipefail
 CURRENT=$(docker ps | grep "$NAME" | awk '{ print $1 }')
 docker stop "$NAME"; docker rm "$NAME"; echo 'no docker container to remove'
 set -eo pipefail
 
-docker build -t tft:test .
+docker build -t "${IMG}" .
 
 docker run \
-	-d \
 	--runtime=nvidia \
 	-p 8080:8080 \
-	-v $(pwd)/../text_generation_data/training_checkpoints/:/opt/text_generation_data/training_checkpoints/ \
 	-v $(pwd)/logs/:/opt/workspace/logs \
 	--name "$NAME" \
-	test:latest $@
+	"${IMG}" $@
